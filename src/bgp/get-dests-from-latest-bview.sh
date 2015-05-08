ba8aea613eb8a0a5c04ece203d49a6762ac5d000
Josef Lusticky <josef@lusticky.cz>
2015-05-08 22:10:13 +0200

    measurements update

--- src/bgp/get-dests-from-latest-bview.sh
+++ src/bgp/get-dests-from-latest-bview.sh
@@ -5,4 +5,9 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
-/usr/local/bin/bgpdump "$1" | grep 'PREFIX' | grep '\.' | sed 's/PREFIX: \(.*\)/\1/' | grep -v '0.0.0.0/0' | uniq
+####
+# Note that the first destination entry is default route 0.0.0.0/0
+# and the latest-bview.gz file also contains subnets such as 10.0.0.0/8 or 192.168.0.0/16,
+# which you may want to remove from the output
+####
+/usr/local/bin/bgpdump "$1" | grep 'PREFIX' | grep '\.' | sed 's/PREFIX: \(.*\)/\1/' | uniq
